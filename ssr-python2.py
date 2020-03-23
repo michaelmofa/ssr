2b45f384c2bef4f3cfaaca0846427cb937e1b4ab
梁广隆 <499508387@qq.com>
2020-03-23 22:29:48 +0800

    no message

--- ssr-python2.py
+++ ssr-python2.py
@@ -4,7 +4,7 @@ import base64, requests, os
 root_dir = os.path.dirname(os.path.abspath(__file__))
 path = root_dir+'/ssr.txt'
 res = root_dir+'/ssr'
-group = '左手zuoshou'
+group = '迈克-ssr'
 group = base64.urlsafe_b64encode(group).strip('=')
 try:
     with open(path,'r') as f:
